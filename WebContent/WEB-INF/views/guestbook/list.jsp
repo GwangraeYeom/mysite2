c11e1e1d41cbffe0d53a63ba8de271da617a76bb
GwangareYeom <ykr1663@gmail.com>
2019-12-23 17:45:48 +0900

    mysite3-guestbook-complete

--- WebContent/WEB-INF/views/guestbook/list.jsp
+++ WebContent/WEB-INF/views/guestbook/list.jsp
@@ -1,4 +1,12 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@page import="com.bigdata2019.mysite.vo.GuestbookVo"%>
+<%@page import="java.util.List"%>
+<%@page import="com.bigdata2019.mysite.repository.GuestbookDao"%>
+<%@page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<%
+	List<GuestbookVo> list = new GuestbookDao().findAll();
+%>  
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,8 +19,8 @@
 		<jsp:include page="/WEB-INF/views/includes/header.jsp"/>
 		<div id="content">
 			<div id="guestbook">
-				<form action="/guestbook" method="post">
-					<input type="hidden" name="a" value="insert">
+				<form action="<%=request.getContextPath() %>/guestbook" method="post">
+					<input type="hidden" name="a" value="insert"/>
 					<table>
 						<tr>
 							<td>이름</td><td><input type="text" name="name"></td>
@@ -28,21 +36,27 @@
 				</form>
 				<ul>
 					<li>
+						<%
+							int totalCount = list.size();
+							int index = 0;
+							for(GuestbookVo vo : list){
+						%>
 						<table>
 							<tr>
-								<td>[4]</td>
-								<td>안대혁</td>
-								<td>2015-11-10 11:22:30</td>
-								<td><a href="">삭제</a></td>
+								<td>[<%=totalCount-index++ %>]</td>
+								<td><%=vo.getName() %></td>
+								<td><%=vo.getRegDate() %></td>
+								<td><a href="<%=request.getContextPath() %>/guestbook?a=deleteform&no=<%=vo.getNo() %>">삭제</a></td>
 							</tr>
 							<tr>
-								<td colspan=4>
-								안녕하세요. ^^;<br>
-								하하하하	
+								<td colspan=4><%=vo.getContents().replace("\n", "<br>")%></td>
 								</td>
 							</tr>
 						</table>
 						<br>
+						<%
+							}
+						%>
 					</li>
 				</ul>
 			</div>
--- src/com/bigdata2019/mysite/controller/GuestbookController.java
+++ src/com/bigdata2019/mysite/controller/GuestbookController.java
@@ -1,11 +1,15 @@
 package com.bigdata2019.mysite.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.bigdata2019.mysite.repository.GuestbookDao;
+import com.bigdata2019.mysite.vo.GuestbookVo;
 import com.bigdata2019.mysite.web.util.WebUtil;
 
 public class GuestbookController extends HttpServlet {
@@ -13,11 +17,39 @@ public class GuestbookController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-
-		WebUtil.forward(request, response, "/WEB-INF/views/guestbook/list.jsp");
+		String action = request.getParameter("a");
+		if("deleteform".equals(action)) {
+			String no = request.getParameter("no");
+			
+			request.setAttribute("no", no);
+			WebUtil.forward(request, response, "/WEB-INF/views/guestbook/deleteform.jsp");
+		}else {
+			WebUtil.forward(request, response, "/WEB-INF/views/guestbook/list.jsp");
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		
+		String action = request.getParameter("a");
+		//sysout으로 콘솔에서 확인 가능
+//		System.out.println(action);
+		
+		if("insert".equals(action)) {
+			String name = request.getParameter("name");
+			String password = request.getParameter("pass");
+			String contents = request.getParameter("content");
+			GuestbookVo vo = new GuestbookVo(name, password, contents);
+			new GuestbookDao().insert(vo);
+			
+			response.sendRedirect(request.getContextPath()+"/guestbook");
+		} else if("delete".equals(action)) {
+			String no = request.getParameter("no");
+			String password = request.getParameter("password");
+			
+			new GuestbookDao().delete(Long.parseLong(no), password);
+			
+			WebUtil.redirect(request, response, request.getContextPath()+"/guestbook");
+		}
 	}
 }
